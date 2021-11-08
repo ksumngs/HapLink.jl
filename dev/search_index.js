45bda5d64efca3e7c182d6d4a7d1e93fe1866c58
Documenter.jl <documenter@juliadocs.github.io>
2021-11-08 23:38:54 +0000

    build based on 6f609f8

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"","page":"Home","title":"Home","text":"CurrentModule = haplink","category":"page"},{"location":"#haplink","page":"Home","title":"haplink","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for haplink.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [haplink]","category":"page"}]
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = HapLink","category":"page"},{"location":"#HapLink","page":"Home","title":"HapLink","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for HapLink.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [HapLink]","category":"page"}]
 }
