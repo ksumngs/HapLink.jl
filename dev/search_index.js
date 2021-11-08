2f6da183b9b37a2f8ef49754f6634de823d14dd1
Documenter.jl <documenter@juliadocs.github.io>
2021-11-08 14:52:36 +0000

    build based on f59ad6a

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = haplink","category":"page"},{"location":"#haplink","page":"Home","title":"haplink","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for haplink.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [haplink]","category":"page"}]
+}
