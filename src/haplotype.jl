306b1874ae6f81c5cada0d9493083f966a5b98d7
Thomas A. Christensen II <25492070+MillironX@users.noreply.github.com>
2021-11-10 09:56:35 -0600

    Separate `Variant` and `Haplotype` into their own files
    Signed-off-by: Thomas A. Christensen II <25492070+MillironX@users.noreply.github.com>

---
+++ src/haplotype.jl
@@ -0,0 +1,17 @@
+include("variant.jl")
+
+export Haplotype
+
+"""
+    Haplotype(mutations::AbstractVector{Variant})
+
+Create an object to describe a group of mutations that appear together. A single [`Variant`]
+can be passed to create a haplotype of a single variant.
+"""
+struct Haplotype
+    mutations::AbstractVector{Variant}
+end #struct
+
+function Haplotype(var::Variant)
+    return Haplotype([var])
+end #function
