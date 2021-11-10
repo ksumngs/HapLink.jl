f28e2a55c97af07b8acfd634cb07f1ddf61df792
Thomas A. Christensen II <25492070+MillironX@users.noreply.github.com>
2021-11-10 12:37:51 -0600

    Move alignment read code to new file
    Signed-off-by: Thomas A. Christensen II <25492070+MillironX@users.noreply.github.com>

--- src/HapLink.jl
+++ src/HapLink.jl
@@ -25,6 +25,7 @@ export linkage
 include("variant.jl")
 include("haplotype.jl")
 include("readcounts.jl")
+include("sequences.jl")
 
 """
     main(args::Dict{String, Any})
@@ -344,101 +345,6 @@ function findsimulatedoccurrences(
 end #function
 
 """
-    myref2seq(aln::Alignment, i::Int)
-
-Replicates the functionality of BioAlignments `ref2seq`, but can handle hard clips
-by effectively removing them for the intent of finding the position.
-"""
-function myref2seq(aln::Alignment, i::Int)
-    if aln.anchors[2].op == OP_HARD_CLIP
-        # Hard clipping was shown on operation 2
-        # (operation 1 is always a start position)
-
-        # Save where the clipping ends
-        alnstart = aln.anchors[2]
-
-        # Declare a new empty array where we can rebuild the alignment
-        newanchors = AlignmentAnchor[]
-
-        # Rebase the start of our new alignment to where the clipping ends
-        push!(newanchors, AlignmentAnchor(
-            0,
-            aln.anchors[1].refpos,
-            OP_START
-        ))
-
-        # Add new anchors
-        for j in 3:(length(aln.anchors)-1)
-            newanchor = AlignmentAnchor(
-                aln.anchors[j].seqpos - alnstart.seqpos,
-                aln.anchors[j].refpos,
-                aln.anchors[j].op
-            )
-            push!(newanchors, newanchor)
-        end #for
-
-        # Package up our new alignment
-        workingalignment = Alignment(newanchors)
-    else
-        # Package up the old alignment if there was no hard clipping
-        workingalignment = aln
-    end #if
-
-    # Check that the requested base is in range
-    if !seqisinrange(workingalignment, i)
-        return (0, OP_HARD_CLIP)
-    end
-
-    # Perform regular alignment search, minus any hard clipping
-    return ref2seq(workingalignment, i)
-
-end #function
-
-function seqisinrange(aln::Alignment, i::Int)
-    reflen = i - first(aln.anchors).refpos
-    seqlen = last(aln.anchors).seqpos - first(aln.anchors).seqpos
-    return seqlen > reflen
-end #function
-
-function firstseqpos(aln::Alignment)
-    return first(aln.anchors).seqpos
-end #function
-
-function lastseqpos(aln::Alignment)
-    return last(aln.anchors).seqpos
-end #function
-
-"""
-    matchvariant(base::Union{NucleotideSeq,DNA,AbstractVector{DNA}}, var::Variant)
-
-Checks if `base` matches the reference or variant expected in `var`, and returns a symbol
-indicating which, if any, it matches.
-
-Returned values can be `:reference` for a reference match, `:alternate` for an alternate
-match, or `:other` for no match with the given variant.
-"""
-function matchvariant(base::NucleotideSeq, var::Variant)
-    refbase = LongDNASeq(var.referencebase)
-    altbase = LongDNASeq(var.alternatebase)
-
-    if base == refbase
-        return :reference
-    elseif base == altbase
-        return :alternate
-    else
-        return :other
-    end #if
-end #function
-
-function matchvariant(base::DNA, var::Variant)
-    return matchvariant(LongDNASeq([base]), var)
-end
-
-function matchvariant(base::AbstractVector{DNA}, var::Variant)
-    return matchvariant(LongDNASeq(base), var)
-end
-
-"""
     linkage(counts::AbstractArray{Int})
 
 Calculates the linkage disequilibrium and Chi-squared significance level of a combination of
@@ -500,19 +406,5 @@ function sumsliced(A::AbstractArray, dim::Int, pos::Int=1)
     i_post = CartesianIndices(size(A)[dim+1:end])
     return sum(A[i_pre, pos, i_post])
 end #function
-
-"""
-    baseatreferenceposition(record::BAM.Record, pos::Int)
-
-Get the base at reference position `pos` present in the sequence of `record`.
-"""
-function baseatreferenceposition(record::BAM.Record, pos::Int)
-    seqpos = myref2seq(BAM.alignment(record), pos)[1]
-    if seqpos > 0 && seqpos < BAM.seqlength(record)
-        return BAM.sequence(record)[seqpos]
-    else
-        return DNA_N
-    end
-end # function
 
 end #module
