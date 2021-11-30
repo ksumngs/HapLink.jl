d532d81012adcfc42b36b6138f5f02141905c4b0
Thomas A. Christensen II <25492070+MillironX@users.noreply.github.com>
2021-11-30 13:17:11 -0600

    Add comments to findsimulatedhaplotypes function
    Signed-off-by: Thomas A. Christensen II <25492070+MillironX@users.noreply.github.com>

--- src/HapLink.jl
+++ src/HapLink.jl
@@ -271,10 +271,15 @@ function findsimulatedhaplotypes(
 
     # TODO: Abstract the simulated haplotype finding into a higher-level function
 
+    # Find every possible pair of variants. These may be valid haplotypes in their own
+    # right, but for right now, we are just going to use them to find linkage between pairs
     variantpairs = combinations(variants, 2)
 
+    # Create a place to store linked pairs and their statistics
     linkedvariantpairhaplotypes = Dict{Haplotype, Matrix{Int}}()
 
+    # Calculate the linkage between every possible variant pair, saving the pair as a
+    # haplotype if it exhibited linkage
     for variantpair in variantpairs
         pairedhaplotype = Haplotype(variantpair)
         hapcount = findsimulatedoccurrences(pairedhaplotype, bamfile, iterations=iterations)
@@ -283,12 +288,20 @@ function findsimulatedhaplotypes(
         end #if
     end #for
 
+    # Use (abuse?) Julia's awesome broadcasting and spatting to get a non-redundant list of
+    # every variant position that exhibited linkage with any other variant position
     linkedvariants = unique(
         cat(map(h -> h.mutations, collect(keys(linkedvariantpairhaplotypes)))..., dims=1)
     )
 
+    # Create a new dict with the basic structure
+    # variant => [all possibly linked variants]
     possiblelinkages = Dict{Variant, AbstractArray{Variant}}()
 
+    # Based on the information of what variants exhibit linkage disequilibrium while in
+    # pairs, for a variant foo, find every other variant that exhibited paired linkage with
+    # foo, and store those other variants in a dict, once again abusing Julia's broadcasting
+    # and splatting
     for variant in linkedvariants
         possiblelinkages[variant] = sort(
             unique(
@@ -306,10 +319,13 @@ function findsimulatedhaplotypes(
         )
     end #for
 
+    # Convert the possible haplotype pairings into non-redundant Haplotype objects
     allvariantcombos = Haplotype.(unique(sort.(values(possiblelinkages))))
 
+    # Set aside a place to put haplotypes that we'll return to the caller
     returnedhaplotypes = Dict{Haplotype, Any}()
 
+    # Calculate the linkage between any new possible haplotypes
     for haplotype in allvariantcombos
         if haskey(linkedvariantpairhaplotypes, haplotype)
             returnedhaplotypes[haplotype] = linkedvariantpairhaplotypes[haplotype]
@@ -320,6 +336,8 @@ function findsimulatedhaplotypes(
             end #if
         end #if
     end #for
+
+    # TODO: Add the single-variant haplotypes back in
 
     return returnedhaplotypes
 
