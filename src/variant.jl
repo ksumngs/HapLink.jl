ksumngs/HapLink.jl

commit 5dd0a3b155962ae7c76f393ddf990e9e6c6a404e

Add a position getter function to Variant type
Signed-off-by: Thomas A. Christensen II <25492070+MillironX@users.noreply.github.com>

src/variant.jl
@@ -118,6 +118,10 @@ function Base.isless(v1::Variant, v2::Variant)
     return v1.chromosome <= v2.chromosome && v1.position < v2.position
 end #function
 
+function varposition(v::Variant)
+    return v.position
+end #function
+
 """
     serialize_yaml(v::Variant)
 

commit 475e6926ee63cbf26af5cd3ed86b47171aeb0f01

Add export statement for varposition function
Fixes: 5dd0a3b155962 ("Add a position getter function to Variant type")
Signed-off-by: Thomas A. Christensen II <25492070+MillironX@users.noreply.github.com>

src/variant.jl
@@ -5,6 +5,7 @@ using DataFrames
 export Variant
 export totaldepth
 export alternatedepth
+export varposition
 
 """
     Variant