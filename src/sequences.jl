ksumngs/HapLink.jl

commit 8227c3a59a3bed110e3662f94d199b981e591a5a

Add containsposition function
Add a function that provides easy access to knowing if a BAM record
contains a particular reference position.

Signed-off-by: Thomas A. Christensen II <25492070+MillironX@users.noreply.github.com>

src/sequences.jl
@@ -214,3 +214,12 @@ function overlap_aligns(
 
     return false
 end #function
+
+"""
+    containsposition(record::BAM.Record, i::Int)
+
+Check to see if reference posision `i` is available in `record`
+"""
+function containsposition(record::BAM.Record, i::Int)
+    return i >= BAM.position(record) && i <= BAM.rightposition(record)
+end

commit 0e30e7b71caa95a7f4749c710e24151d9cbed772

Add export statement for containsposition function
Fixes: 8227c3a59a3b ("Add containsposition function")
Signed-off-by: Thomas A. Christensen II <25492070+MillironX@users.noreply.github.com>

src/sequences.jl
@@ -9,6 +9,7 @@ export mutate
 export variant_positions_match
 export overlap_inrange
 export overlap_aligns
+export containsposition
 
 """
     myref2seq(aln::Alignment, i::Int)