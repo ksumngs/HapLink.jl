ksumngs/HapLink.jl

commit 594fc96be675a19cb9c83ff9a8d90df848a88214

Add function for parsing `Variant`s from VCF strings
Signed-off-by: Thomas A. Christensen II <25492070+MillironX@users.noreply.github.com>

src/variant.jl
@@ -42,11 +42,10 @@ Variant(chromosome::String, position::Int, identifier::String, referencebase::Nu
 Variant(data::DataFrameRow)
 ```
 
-`Variant`s can be created from the default constructor, or via a row generated by
-[`transformbamcounts`](@ref).
+`Variant`s can be created from the default constructor, a VCF formatted string, or via a row
+generated by [`transformbamcounts`](@ref).
 
 See also [`Haplotype`](@ref)
-
 """
 struct Variant
     chromosome::String
@@ -98,6 +97,33 @@ function Variant(vardict::Dict{String,Any})
     altseq = LongDNASeq(altbase)
 
     return Variant(region, pos, id, refseq, altseq, qual, filter, Dict())
+end #function
+
+function Variant(varstring::AbstractString)
+    # Split the tab fields
+    varfields = split(varstring, "\t")
+
+    # Extract the basic fields
+    region = varfields[1]
+    pos = varfields[2]
+    id = varfields[3]
+    refbase = varfields[4]
+    altbase = varfields[5]
+    qual = varfields[6]
+    filter = Symbol(varfields[7])
+
+    # Convert the info fields into a dictionary
+    info = Dict{String,Any}
+    infofields = split(varfields[8], ";")
+    for f in infofields
+        infofield = split(f, "=")
+        info[string(infofield[1])] = infofield[2]
+    end #for
+
+    refseq = LongDNASeq(refbase)
+    altseq = LongDNASeq(altbase)
+
+    return Variant(region, pos, id, refseq, altseq, qual, filter, info)
 end #function
 
 function Base.show(io::IO, v::Variant)

commit bb5371b4c1a80c28c36b7f3f39a72a4c019de35e

Fix type safety for Variant VCF parsing
Fixes: 594fc96be675 ("Add function for parsing `Variant`s from VCF strings")
Signed-off-by: Thomas A. Christensen II <25492070+MillironX@users.noreply.github.com>

src/variant.jl
@@ -105,19 +105,19 @@ function Variant(varstring::AbstractString)
 
     # Extract the basic fields
     region = varfields[1]
-    pos = varfields[2]
+    pos = parse(Int, varfields[2])
     id = varfields[3]
     refbase = varfields[4]
     altbase = varfields[5]
-    qual = varfields[6]
+    qual = parse(Float64, varfields[6])
     filter = Symbol(varfields[7])
 
     # Convert the info fields into a dictionary
-    info = Dict{String,Any}
+    info = Dict{String,Any}()
     infofields = split(varfields[8], ";")
     for f in infofields
         infofield = split(f, "=")
-        info[string(infofield[1])] = infofield[2]
+        info[infofield[1]] = parse(Int64, infofield[2])
     end #for
 
     refseq = LongDNASeq(refbase)