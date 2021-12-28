56900ed3891999b12ae66464da8e85ee57c9336e
Thomas A. Christensen II <25492070+MillironX@users.noreply.github.com>
2021-12-28 17:24:16 -0600

    Add Dict conversion function for HaplotypeMeta pair
    Signed-off-by: Thomas A. Christensen II <25492070+MillironX@users.noreply.github.com>

--- src/haplotypemeta.jl
+++ src/haplotypemeta.jl
@@ -4,3 +4,13 @@ Base.@kwdef mutable struct HaplotypeMeta
     significance::Union{Float64,Nothing} = nothing
     name::Union{String,Nothing} = nothing
 end #struct
+
+function Dict(hp::Pair{Haplotype,HaplotypeMeta})
+    return Dict(
+        "frequency" => hp[2].frequency,
+        "linkage" => hp[2].linkage,
+        "significance" => hp[2].significance,
+        "name" => hp[2].name,
+        "snps" => Dict.(hp[1].mutations)
+    )
+end #function
