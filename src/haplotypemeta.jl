ksumngs/HapLink.jl

commit ae0daa01de6b65a41d12c2495f50a3e6172802dd

Add HaplotypeMeta type
Signed-off-by: Thomas A. Christensen II <25492070+MillironX@users.noreply.github.com>

src/haplotypemeta.jl
@@ -1,0 +1,6 @@
+@kwdef mutable struct HaplotypeMeta
+    frequency::Union{Float64,Nothing} = nothing
+    linkage::Union{Float64,Nothing} = nothing
+    significance::Union{Float64,Nothing} = nothing
+    name::Union{String,Nothing} = nothing
+end #struct

src/HapLink.jl
@@ -25,6 +25,7 @@ include("readcounts.jl")
 include("sequences.jl")
 include("variantcalling.jl")
 include("haplotypecalling.jl")
+include("haplotypemeta.jl")
 
 function parse_arguments()
     s = ArgParseSettings(;

commit e69c61681e9281664b677e0f2d9fa7106a900cc1

Fix `kwdef` reference
Fixes: ae0daa01de6b ("Add HaplotypeMeta type")
Signed-off-by: Thomas A. Christensen II <25492070+MillironX@users.noreply.github.com>

src/haplotypemeta.jl
@@ -1,4 +1,4 @@
-@kwdef mutable struct HaplotypeMeta
+Base.@kwdef mutable struct HaplotypeMeta
     frequency::Union{Float64,Nothing} = nothing
     linkage::Union{Float64,Nothing} = nothing
     significance::Union{Float64,Nothing} = nothing