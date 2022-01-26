d5e804d7fae02b11e7937b76f9a821520fb42673
Thomas A. Christensen II <25492070+MillironX@users.noreply.github.com>
2022-01-26 13:14:43 -0600

    Add function for finding the depth of a snp in a set of SAM/BAM records
    Signed-off-by: Thomas A. Christensen II <25492070+MillironX@users.noreply.github.com>

--- src/snpstats.jl
+++ src/snpstats.jl
@@ -1,6 +1,7 @@
 using XAM
 
 export doescontain
+export depth
 
 """
     doescontain(snp::SNP, rec::Union{SAM.Record,BAM.Record})
@@ -26,4 +27,52 @@ false
 function doescontain(snp::SNP, rec::Union{SAM.Record,BAM.Record})
     return doescontain(snp.location, rec) &&
            first(basesat(snp.location, rec)) == snp.altbase
+end #function
+
+"""
+    depth(snp::SNP, reads::AbstractVector{T}) where T <: Union{SAM.Record,BAM.Record}
+
+Calculate the number of times `snp` is represented within `reads`
+
+# Example
+
+```jldoctest
+julia> using BioSymbols, GenomicFeatures, XAM
+
+julia> # All SAM spec example sequences, with an added mutation at position 17 in reads 1 and 4
+
+julia> samrecords = SAM.Record.([
+           "r001\\t99\\tref\\t7\\t30\\t8M2I4M1D3M\\t=\\t37\\t39\\tTTAGATAAAGGACACTG\\t*",
+           "r003\\t0\\tref\\t9\\t30\\t5S6M\\t*\\t0\\t0\\tGCCTAAGCTAA\\t*\\tSA:Z:ref,29,-,6H5M,17,0;",
+           "r004\\t0\\tref\\t16\\t30\\t6M14N5M\\t*\\t0\\t0\\tACAGCTTCAGC\\t*",
+           "r003\\t2064\\tref\\t29\\t17\\t6H5M\\t*\\t0\\t0\\tTAGGC\\t*\\tSA:Z:ref,9,+,5S6M,30,1;",
+           "r001\\t147\\tref\\t37\\t30\\t9M\\t=\\t7\\t-39\\tCAGCGGCAT\\t*\\tNM:i:1"
+       ]);
+
+julia> depth(SNP("ref", 17, DNA_T, DNA_C), samrecords)
+2
+```
+"""
+function depth(snp::SNP, reads::AbstractVector{SAM.Record})
+    if any(contains.(SAM.cigar.(reads), "P"))
+        @warn """
+            One or more of these reads contains a 'P' CIGAR operation, which is not yet
+            supported by BioAlignments.jl. These reads will be ignored.
+            """
+        cleanedreads = filter(r -> !contains(SAM.cigar(r), "P"), reads)
+        return count(r -> doescontain(snp, r), cleanedreads)
+    end #if
+    return count(r -> doescontain(snp, r), reads)
+end #function
+
+function depth(snp::SNP, reads::AbstractVector{BAM.Record})
+    if any(contains.(BAM.cigar.(reads), "P"))
+        @warn """
+            One or more of these reads contains a 'P' CIGAR operation, which is not yet
+            supported by BioAlignments.jl. These reads will be ignored.
+        """
+        cleanedreads = filter(r -> !contains(BAM.cigar(r), "P"), reads)
+        return count(r -> doescontain(snp, r), cleanedreads)
+    end #if
+    return count(r -> doescontain(snp, r), reads)
 end #function
