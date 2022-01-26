c887680bc185958a32de8b4572c32281a290fd91
Thomas A. Christensen II <25492070+MillironX@users.noreply.github.com>
2022-01-26 12:40:35 -0600

    Correct test position in doescontain for SNPs
    Signed-off-by: Thomas A. Christensen II <25492070+MillironX@users.noreply.github.com>

--- src/snpstats.jl
+++ src/snpstats.jl
@@ -16,7 +16,7 @@ julia> # SAM spec example record, with an added mutation at postion 12
 
 julia> samrecord = SAM.Record("r001\\t99\\tref\\t7\\t30\\t8M2I4M1D3M\\t=\\t37\\t39\\tTTAGACAAAGGATACTG\\t*");
 
-julia> doescontain(SNP("ref", 6, DNA_T, DNA_C), samrecord)
+julia> doescontain(SNP("ref", 12, DNA_T, DNA_C), samrecord)
 true
 
 julia> doescontain(SNP("ref", 17, DNA_T, DNA_A), samrecord)
