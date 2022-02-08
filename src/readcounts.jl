a3f6e1ad5dbbe1ed3dfaea33a20035f8333f37c1
Thomas A. Christensen II <25492070+MillironX@users.noreply.github.com>
2022-02-08 13:56:26 -0600

    Add a function to find all possible snps
    Signed-off-by: Thomas A. Christensen II <25492070+MillironX@users.noreply.github.com>

--- src/readcounts.jl
+++ src/readcounts.jl
@@ -2,7 +2,49 @@ using DataFrames
 using Pkg.Artifacts
 
 export countbasestats
+export possible_snps
 export transformbamcounts
+
+"""
+    possible_snps(reference::AbstractVector{FASTA.Record})
+
+Get an array of every possible `SNP` that could be mutated from `reference`.
+
+# Example
+
+```jldoctest
+julia> using BioSymbols, FASTX, GenomicFeatures
+
+julia> first(possible_snps([FASTA.Record(">ref\\nAGCATGTTAGATAAGATAGCTGTGCTAGTAGGCAGTCAGCGCCAT")]), 2)
+2-element Vector{SNP}:
+ SNP{DNA}(GenomicFeatures.Interval{Nothing}:
+  sequence name: ref
+  leftmost position: 1
+  rightmost position: 1
+  strand: .
+  metadata: nothing, DNA_A, DNA_C)
+ SNP{DNA}(GenomicFeatures.Interval{Nothing}:
+  sequence name: ref
+  leftmost position: 1
+  rightmost position: 1
+  strand: .
+  metadata: nothing, DNA_A, DNA_G)
+```
+"""
+function possible_snps(reference::AbstractVector{FASTA.Record})
+    snps = SNP[]
+    for chrom in reference
+        for i in 1:FASTA.seqlen(chrom)
+            alph = typeof(FASTA.sequence(chrom)[i]) == DNA ? ACGT : ACGU
+            for N in alph
+                if N != FASTA.sequence(chrom)[i]
+                    push!(snps, SNP(FASTA.identifier(chrom), i, FASTA.sequence(chrom)[i], N))
+                end #if
+            end #for
+        end #for
+    end #for
+    return snps
+end #function
 
 """
     countbasestats(bamfile::String, reffile::String)
