d8df47220480b0b9ad74b9ab59e87be102e7380a
Thomas A. Christensen II <25492070+MillironX@users.noreply.github.com>
2022-02-14 08:27:42 -0600

    Remove samstrings definition comment
    Signed-off-by: Thomas A. Christensen II <25492070+MillironX@users.noreply.github.com>

--- src/intervalstats.jl
+++ src/intervalstats.jl
@@ -8,19 +8,6 @@ export doescontain
 export fractional_position
 export mean_quality
 
-#=
-samstrings = [
-    "r001\t99\tref\t7\t30\t8M2I4M1D3M\t=\t37\t39\tTTAGATAAAGGATACTG\t*",
-    "r002\t0\tref\t9\t30\t3S6M1P1I4M\t*\t0\t0\tAAAAGATAAGGATA\t*",
-    "r003\t0\tref\t9\t30\t5S6M\t*\t0\t0\tGCCTAAGCTAA\t*\tSA:Z:ref,29,-,6H5M,17,0;",
-    "r004\t0\tref\t16\t30\t6M14N5M\t*\t0\t0\tATAGCTTCAGC\t*",
-    "r003\t2064\tref\t29\t17\t6H5M\t*\t0\t0\tTAGGC\t*\tSA:Z:ref,9,+,5S6M,30,1;",
-    "r001\t147\tref\t37\t30\t9M\t=\t7\t-39\tCAGCGGCAT\t*\tNM:i:1"
-]
-
-refstring = "AGCATGTTAGATAAGATAGCTGTGCTAGTAGGCAGTCAGCGCCAT"
-=#
-
 """
     basesat(int::GenomicFeatures.Interval, rec::Union{SAM.Record, BAM.Record})
 
