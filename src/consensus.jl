dc9eebc828a6890bf81e0ef662cf1a419aa51f92
Thomas A. Christensen II <25492070+MillironX@users.noreply.github.com>
2022-04-28 09:47:01 -0500

    Replace logic in consensus function with call to consensus_variants
    Signed-off-by: Thomas A. Christensen II <25492070+MillironX@users.noreply.github.com>

--- src/consensus.jl
+++ src/consensus.jl
@@ -6,23 +6,7 @@ Generate the consensus sequence of `refseq` when mutated by `vars`, excluding an
 """
 function consensus(refseq::LongDNASeq, vars::AbstractArray{Variant}; freq::Float64=0.5)
     consensus_seq = copy(refseq)
-
-    for i in eachindex(refseq)
-        containingvars = filter(v -> varposition(v) == i, vars)
-        sort!(containingvars; lt=(x, y) -> alternatedepth(x) < alternatedepth(y))
-
-        if length(containingvars) < 1
-            continue
-        end #if
-
-        mostvar = last(containingvars)
-
-        if (alternatedepth(mostvar) / totaldepth(mostvar)) > freq
-            consensus_seq = mutate(consensus_seq, Haplotype([mostvar]))
-        end #if
-    end #for
-
-    return consensus_seq
+    return mutate(consensus_seq, Haplotype(consensus_variants(vars; freq=freq)))
 end #function
 
 """
