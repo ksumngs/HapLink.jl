81bd979710ea7bfbc61f5a73eef1c79a34fbb31d
Thomas A. Christensen II <25492070+MillironX@users.noreply.github.com>
2022-04-28 12:58:37 -0500

    Add hash function for haplotypes
    Signed-off-by: Thomas A. Christensen II <25492070+MillironX@users.noreply.github.com>

--- test/haplotype.jl
+++ test/haplotype.jl
@@ -3,3 +3,5 @@
 ])
 @test Haplotype([var1, var3]) == Haplotype([var3, var1])
 @test Haplotype([var1, var3]) != Haplotype([var1])
+@test hash(Haplotype([var1, var3])) == hash(Haplotype([var1, var3]))
+@test hash(Haplotype([var1, var3])) == hash(Haplotype([var3, var1]))
