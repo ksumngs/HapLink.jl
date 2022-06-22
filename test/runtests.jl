2f67ad7b62d9f70fc78c757ef84171a3a11341af
Thomas A. Christensen II <25492070+MillironX@users.noreply.github.com>
2022-06-22 11:10:26 -0500

    Add a more fully fleshed-out sam conversion to test suite

--- test/runtests.jl
+++ test/runtests.jl
@@ -10,15 +10,30 @@ using BioGenerics: leftposition
 using BioSequences: BioSequence, @dna_str, ungap!
 using Documenter: DocMeta, doctest
 using HapLink
+using Random: randstring, seed!
 using SequenceVariation: Variant, Variation, variations
 using Test
 using XAM: SAM, BAM
 
+seed!(1)
+
 const DNA_MODEL = AffineGapScoreModel(EDNAFULL; gap_open=-12, gap_extend=-3)
 align(a::BioSequence, b::BioSequence) = pairalign(GlobalAlignment(), a, b, DNA_MODEL).aln
 function sam(a::PairwiseAlignment)
+    qname = randstring()
+    flag = 0x00
+    rname = "REFERENCE"
+    pos = a.a.aln.firstref
+    mapq = 255
+    cig = cigar(a.a.aln)
+    rnext = "*"
+    pnext = 0
+    tlen = 0
+    seq = a.a.seq
+    qual = String(Char.(0x22:(0x22 + UInt8(length(seq)))))
+    tag = "Xs:Z:HapLink"
     return SAM.Record(
-        "*\t0\tREFERENCE\t$(a.a.aln.firstref)\t255\t$(cigar(a.a.aln))\t*\t0\t0\t$(a.a.seq)\t*\tXs:Z:HapLink",
+        join([qname, flag, rname, pos, mapq, cig, rnext, pnext, tlen, seq, qual, tag], "\t")
     )
 end #function
 
