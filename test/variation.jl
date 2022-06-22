59acbc9d144d2764d4050daee7971b28e0670b85
Thomas A. Christensen II <25492070+MillironX@users.noreply.github.com>
2022-06-22 14:20:36 -0500

    Refactor variation tests to use more verbose constant names

--- test/variation.jl
+++ test/variation.jl
@@ -1,24 +1,28 @@
+const SUBSTITUTION = Variation(REFERENCE, "G8A")
+const DELETION = Variation(REFERENCE, "Δ25-26")
+const INSERTION = Variation(REFERENCE, "19GTC")
+
 @testset "variation" begin
     @testset "variations" begin
         # Test that the types of variations is correct
         @test eltype(variations(VARIANTS)) <: Variation
 
         # Test for each type of Variation that should be present in the list
-        @test Variation(REFERENCE, "G8A") in variations(VARIANTS)
-        @test Variation(REFERENCE, "Δ25-26") in variations(VARIANTS)
-        @test Variation(REFERENCE, "19GTC") in variations(VARIANTS)
+        @test SUBSTITUTION in variations(VARIANTS)
+        @test DELETION in variations(VARIANTS)
+        @test INSERTION in variations(VARIANTS)
     end #testset
 
     @testset "seqpos" begin
         # Test that the positions are what we expect
-        @test seqpos(VARIATIONS[1], ALIGNMENTS[2]) == 8
-        @test seqpos(VARIATIONS[5], ALIGNMENTS[3]) == 24
-        @test seqpos(VARIATIONS[end - 2], ALIGNMENTS[end]) == 19
+        @test seqpos(SUBSTITUTION, ALIGNMENTS[2]) == 8
+        @test seqpos(DELETION, ALIGNMENTS[3]) == 24
+        @test seqpos(INSERTION, ALIGNMENTS[end]) == 19
     end #testset
 
     @testset "relativepos" begin
-        @test relativepos(VARIATIONS[1], SAMS[2]) == 8 / 33
-        @test relativepos(VARIATIONS[5], SAMS[3]) == 24 / 33
-        @test relativepos(VARIATIONS[end - 2], SAMS[end]) == 19 / 33
+        @test relativepos(SUBSTITUTION, SAMS[2]) == 8 / 33
+        @test relativepos(DELETION, SAMS[3]) == 24 / 33
+        @test relativepos(INSERTION, SAMS[end]) == 19 / 33
     end #testset
 end #testset
