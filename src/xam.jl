8f4f21114561ca5c36c1cf189b90f2296295119f
Thomas A. Christensen II <25492070+MillironX@users.noreply.github.com>
2022-06-23 10:00:39 -0500

    Add _xam_reader_switch function

--- src/xam.jl
+++ src/xam.jl
@@ -1,3 +1,7 @@
 function _xam_record_switch(r::Union{Type{SAM.Record},Type{BAM.Record}})
     return r <: SAM.Record ? :SAM : :BAM
 end
+
+function _xam_reader_switch(r::Union{Type{SAM.Reader},Type{BAM.Reader}})
+    return r <: SAM.Reader ? :SAM : :BAM
+end #function
