725910a479441aca4e1dfc01a1e8dac0b842a53a
Thomas A. Christensen II <25492070+MillironX@users.noreply.github.com>
2022-06-28 11:31:46 -0500

    Add a function to find the bam index

--- src/xam.jl
+++ src/xam.jl
@@ -24,3 +24,17 @@ end #function
         return Interval($XAM.refname(r), $XAM.position(r), $XAM.rightposition(r))
     end #quote
 end #function
+
+FilePaths.@compat function _find_bam_index(bam::AbstractPath)
+    # Check for a '.bam.bai' extension
+    bai = Path("$bam.bai")
+    isfile(bai) && return bai
+
+    # Check for a swapped extension
+    bai = Path("$(first(splittext(bam))).bai")
+    isfile(bai) && return bai
+
+    # Couldn't fine an index
+    @warn "Couldn't find an index file for $bam. Analysis may be significantly slower."
+    return nothing
+end #function
