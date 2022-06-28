308d72196c22632db5db5ce21839996955518aa1
Thomas A. Christensen II <25492070+MillironX@users.noreply.github.com>
2022-06-28 11:18:11 -0500

    Add _issam function to detect alignment file format

--- src/xam.jl
+++ src/xam.jl
@@ -2,6 +2,18 @@ function _xam_record_switch(r::Union{Type{SAM.Record},Type{BAM.Record}})
     return r <: SAM.Record ? :SAM : :BAM
 end
 
-function _xam_reader_switch(r::Union{Type{SAM.Reader},Type{BAM.Reader}})
+function _xam_reader_switch(r::Union{Type{S},Type{B}}) where {S<:SAM.Reader,B<:BAM.Reader}
     return r <: SAM.Reader ? :SAM : :BAM
+end #function
+
+function _issam(file::Union{AbstractPath,AbstractString})
+    for line in readlines(string(file))
+        if startswith(line, '@')
+            if startswith(line, "@SQ")
+                return true
+            end #if
+        end #if
+    end #for
+
+    return false
 end #function
