3337ea14f4d3d07f18758e017480971a62f1f5b5
Thomas A. Christensen II <25492070+MillironX@users.noreply.github.com>
2022-07-18 16:38:02 -0500

    Add VariationCall type

---
+++ src/variationcall.jl
@@ -0,0 +1,30 @@
+"""
+    VariationCall
+
+Represents a `Variation` that is supported by aligned reads with sufficient metadata to
+support or refute its validity. It is designed to be converted into a line in [Variant Call
+Format](https://github.com/samtools/hts-specs#variant-calling-data-files), or a
+[`VCF.Record`](https://rasmushenningsson.github.io/VariantCallFormat.jl/stable/vcf-bcf/).
+
+# Fields
+- `variation::Variation`: The `Variation` of this call
+- `quality::Union{Nothing,Number}`: Phred quality of the basecall for `variation`
+- `filter::Union{Nothing,Vector{String}}`: Indicator if `variation` has passed filters and
+  is actually a variant call, or a list of criteria that have failed it
+- `depth::Union{Nothing,UInt}`: The number of reads that cover `leftposition(variation)`
+- `strandbias::Union{Nothing,Float64}`: The fraction of times `variation` appears on a
+  positive strand
+- `altdepth::Union{Nothing,UInt}`: The number of types `variation` occurs
+- `readpos::Union{Nothing,UInt}`: The averagerelative position of `variation` in each read
+- `pvalue::Union{Nothing,Float64}`: The Fisher's Exact Test ``p``-value of this call
+"""
+Base.@kwdef struct VariationCall
+    variation::Variation
+    quality::Union{Nothing,Number} = nothing
+    filter::Union{Nothing,Vector{String}} = nothing
+    depth::Union{Nothing,UInt} = nothing
+    strandbias::Union{Nothing,Float64} = nothing
+    altdepth::Union{Nothing,UInt} = nothing
+    readpos::Union{Nothing,Float64} = nothing
+    pvalue::Union{Nothing,Float64} = nothing
+end #struct
