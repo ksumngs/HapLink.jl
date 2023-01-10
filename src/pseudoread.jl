d73a9fdc567e287f01b3374671743cfdd56064d4
Thomas A. Christensen II <25492070+MillironX@users.noreply.github.com>
2023-01-10 09:10:22 -0600

    Add doctests for overlap_inrange

--- src/pseudoread.jl
+++ src/pseudoread.jl
@@ -205,6 +205,24 @@ end #function
 
 Returns `true` if the overlap between `x` and `y` is within `overlap_min` and `overlap_max`,
 returns `false` otherwise
+
+# Example
+```jldoctest
+julia> using BioSequences, SequenceVariation
+
+julia> ps1 = Pseudoread(1, 100, Variant(dna"A", Variation{LongDNA{4}, DNA}[]));
+
+julia> ps2 = Pseudoread(50, 150, Variant(dna"A", Variation{LongDNA{4}, DNA}[]));
+
+julia> overlap_inrange(ps1, ps2, 1, 500)
+true
+
+julia> overlap_inrange(ps1, ps2, 75, 500)
+false
+
+julia> overlap_inrange(ps1, ps2, 1, 25)
+false
+```
 """
 function overlap_inrange(
     x::Pseudoread, y::Pseudoread, overlap_min::Integer, overlap_max::Integer
