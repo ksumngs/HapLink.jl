54331fb2bc9f283d4ebcc3c6cb67853ff7a75551
Thomas A. Christensen II <25492070+MillironX@users.noreply.github.com>
2023-03-25 21:46:15 -0500

    test: Add tests for linkage statistics

--- test/haplotypecalling.jl
+++ test/haplotypecalling.jl
@@ -1,6 +1,10 @@
 @testitem "Linkage disequilibrium" begin
-    using HapLink: linkage
+    using HapLink: linkage, _correlation_coeff
+
+    genotypes = [474 142; 611 773]
 
     # https://pbgworks.org/sites/pbgworks.org/files/measuresoflinkagedisequilibrium-111119214123-phpapp01_0.pdf
-    @test trunc(linkage([474 142; 611 773]); digits=4) ≈ 0.0699
+    @test trunc(linkage(genotypes); digits=4) ≈ 0.0699
+    @test trunc(_correlation_coeff(genotypes); digits=2) ≈ 0.30
+    @test trunc(significance(genotypes); digits=4) <= 0.0001
 end #testitem
