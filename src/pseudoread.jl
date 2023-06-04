0b5a5c398f5e01e7d63cefc298d3a2b6d9a21a76
Thomas A. Christensen II <25492070+MillironX@users.noreply.github.com>
2023-06-04 16:20:54 -0500

    docs: Add docstring for Pseudoread

--- src/pseudoread.jl
+++ src/pseudoread.jl
@@ -1,3 +1,10 @@
+"""
+    Pseudoread(startpos::Integer, endpos::Integer, read::Haplotype)
+    Pseudoread(query::Union{SAM.Record,BAM.Record}, reference::NucleotideSeq)
+
+A stand-in struct for a sequencing read (real or imaginary) represented by its position
+and differences relative to a reference sequence.
+"""
 struct Pseudoread
     startpos::UInt64
     endpos::UInt64
