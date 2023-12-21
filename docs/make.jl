7c2f787c7576dedc8e52153d4e9f86f7a930e4f9
Thomas A. Christensen II <25492070+MillironX@users.noreply.github.com>
2023-12-21 16:18:49 -0700

    fix: Remove repo keyword from docs script
    Documenter.jl changed the way it handles links, so we did too.

--- docs/make.jl
+++ docs/make.jl
@@ -6,7 +6,6 @@ DocMeta.setdocmeta!(HapLink, :DocTestSetup, :(using HapLink); recursive=true)
 makedocs(;
     modules=[HapLink],
     authors="Thomas A. Christensen II, Kansas State University, and contributors",
-    repo="https://github.com/ksumngs/HapLink.jl/blob/{commit}{path}#{line}",
     sitename="HapLink.jl",
     format=Documenter.HTML(;
         prettyurls=get(ENV, "CI", "false") == "true",
